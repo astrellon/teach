0ed59769d33d8030448d978390d97ac794558386
Astrellon <sovereign250@gmail.com>
2015-05-13 19:15:40 +1000

    - Added First Unity project.

---
+++ first/Assets/MoveTheCube.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTheCube : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Add code here
+		// So here we're moving the cube to the right every frame, however we don't want to do that
+		// independantly of the framerate otherwise it'll move really fast. So we multiply the time difference
+		// between each frame then after 1 second the cube will have moved by 1 unit.
+        //transform.position += Vector3.right * Time.deltaTime;
+
+		if (Input.GetKey(KeyCode.A))
+		{
+			transform.position -= Vector3.right * Time.deltaTime;
+		}
+		else if (Input.GetKey(KeyCode.D))
+		{
+			transform.position += Vector3.right * Time.deltaTime;
+		}
+
+	}
+}
