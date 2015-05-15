c2f3059ebe352e7a271b0754546f1f06e64c569e
Astrellon <sovereign250@gmail.com>
2015-05-15 21:08:29 +1000

    - Added new first lesson and new third lesson.

---
+++ first/Assets/HelloWorld.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+	// Use this for initialization
+	void Start () 
+    {
+        int age = 24;
+        Debug.Log(age);
+
+        int newAge = AddOne(age);
+        Debug.Log(age);
+        Debug.Log(newAge);
+	}
+	
+    int AddOne(int input)
+    {
+        return input + 1;
+    }
+}
---
+++ third/Assets/Scripts/Cannon.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Cannon : MonoBehaviour
+{
+    public GameObject CannonBallPrefab;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (Input.GetKeyDown(KeyCode.Space))
+        {
+            var newCannonBall = Instantiate(CannonBallPrefab, Vector3.zero, Quaternion.identity);
+        }
+	}
+}
